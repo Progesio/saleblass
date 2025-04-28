1ef80adc227d1eafa1f49caba8c003e2ca7a1ce7
Agustinus <57866141+lgarin211@users.noreply.github.com>
2025-04-28 14:47:51 +0000

    modified:   laravel/resources/views/Produk/blink.blade.php

--- laravel/resources/views/Produk/blink.blade.php
+++ laravel/resources/views/Produk/blink.blade.php
@@ -98,7 +98,7 @@
             </div>
 
 
-             <h1 class="card bg-light text-center text-white mt-5 mb-5'>">
+             <h1 class="card bg-light text-center text-success mt-5 mb-5'>">
                 Saat ini Pembayaran Otamatis Sedang dalam Maintenance, silahkan hubungi kami melalui WhatsApp untuk melakukan pembayaran manual.
                 <hr>
                 <a href='https://wa.me/6282111424592?text=' + encodeURIComponent('Saya hendak membeli paket ... ') class='btn btn-success'>Hubungi Kami</a>
