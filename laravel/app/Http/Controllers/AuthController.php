61a9d548acc88b8a9a2e4ac743ccbc3bca31b790
Agustinus <57866141+lgarin211@users.noreply.github.com>
2025-04-28 14:18:27 +0000

    modified:   laravel/app/Http/Controllers/AuthController.php

--- laravel/app/Http/Controllers/AuthController.php
+++ laravel/app/Http/Controllers/AuthController.php
@@ -7,6 +7,7 @@ use App\Models\UserWaPoliester;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Http;
 
 class AuthController extends Controller
 {
