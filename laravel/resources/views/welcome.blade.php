c7c8cbd71af5352485482b770935469594aad358
Agustinus <57866141+lgarin211@users.noreply.github.com>
2025-04-28 13:48:14 +0000

    modified:   laravel/resources/views/welcome.blade.php

--- laravel/resources/views/welcome.blade.php
+++ laravel/resources/views/welcome.blade.php
@@ -3,8 +3,20 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Landing Page - Pilih Paket</title>
-    <!-- Link ke Bootstrap CSS -->
+    <title>Progesio Whatsapp Blass</title>
+
+    <meta name="description" content="Progesio Whatsapp Blass">
+    <meta name="keywords" content="Progesio, Whatsapp Blass, Progesio Whatsapp Blass">
+    <meta name="author" content="Progesio">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="author" content="Progesio">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    {{-- icon --}}
+    <link rel="icon" href="https://progesio.my.id/icon.png" type="image/x-icon">
+
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="{{ url('bootstrap.min (1).css') }}">
     <link rel="stylesheet" href="{{ url('blis.css') }}">
